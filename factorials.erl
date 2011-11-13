1404e9f62b41f2532b353d1fa8e2df9e3ee91deb
Eren Guven <erenguven0@gmail.com>
2011-11-14 00:49:26 +0200

    Still a git noob. Old changes were not commited.

--- factorials.erl
+++ factorials.erl
@@ -1,8 +1,10 @@
 -module(factorials).
 -export([fac/1,fact/2]).
 
+% recursive factorial
 fac(0) -> 1;
 fac(N) -> N * fac(N-1).
 
+% tail-recursive factorial
 fact(1,Acc) -> Acc;
 fact(N,Acc) -> fact(N-1,N*Acc).
--- quicksort.erl
+++ quicksort.erl
@@ -9,11 +9,15 @@
 %    true -> lists:append([L1,[N],L2])
 %  end.
 
+% combiner function
+% [X],X,[X] -> [X]
 combine([],N,[]) -> [N];
 combine([],N,L2) -> [N|L2];
 combine(L1,N,[]) -> lists:append(L1,[N]);
 combine(L1,N,L2) -> lists:append([L1,[N],L2]).
 
+% quicksort with HEAD as pivot
+% filter written insead of lists:partition
 qsort([]) -> [];
 qsort([H|T]) ->
   combine(qsort(lists:filter(fun(X) -> X =< H end, T)),
